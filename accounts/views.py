407356f672ea474779a262c4b52334c4acdcbd00
Elyorbek <shamolkabierkin@gmail.com>
2024-05-28 22:42:45 +0500

    project asosiy fayllari yaratildi

---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from .forms import UserForm, ProfileForm
+
+def profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('profile')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'accounts/profile.html', {'user_form': user_form, 'profile_form': profile_form})
